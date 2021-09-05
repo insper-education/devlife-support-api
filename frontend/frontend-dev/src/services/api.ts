8957e308df062c9ee5cecd1efba85148a46b4eea
cicerotcv <elm.tiago@gmail.com>
2021-09-05 02:15:41 -0300

    else case

--- frontend/frontend-dev/src/services/api.ts
+++ frontend/frontend-dev/src/services/api.ts
@@ -9,6 +9,10 @@ function preRequest(config: AxiosRequestConfig) {
   const user = retrieveUser();
   if (user) {
     config.headers = { ...config.headers, Authorization: `Token ${user.token}` };
+  } else {
+    // handle "not signed in" case for current user
+    // what should happen if current user tries to make an 
+    // authenticaded api call without credentials?
   }
   return config;
 }
