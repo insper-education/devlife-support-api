9a63b01958529bd54411631a6be5e883a03397e7
Andrew Kurauchi <andrew.kurauchi@gmail.com>
2021-10-05 18:14:08 -0300

    Add frontend build and fix img static path

--- frontend/frontend-dev/src/fragments/SignInContainer/index.tsx
+++ frontend/frontend-dev/src/fragments/SignInContainer/index.tsx
@@ -12,7 +12,11 @@ const SignInContainer = ({ children }: ISignInContainerProps) => {
     <div className="w-full min-h-screen px-2 flex flex-col lg:flex-row justify-center lg:justify-around items-center bg-gray-100">
       <div className="hidden md:block mb-8 lg:pr-24">
         <h1 className="text-8xl text-primary mb-4">Developer Life</h1>
-        <img src="/img/insper.png" className="h-16" />
+        <img
+          src="/static/static/img/insper.png"
+          className="h-16"
+          alt="Insper Logo"
+        />
       </div>
       <div className="max-w-xs rounded shadow">
         <h2 className="text-3xl uppercase px-6 py-4 bg-primary text-white rounded-t text-center md:hidden">
