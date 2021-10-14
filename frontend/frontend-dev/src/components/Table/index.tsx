1bbddce5085ccf32960f496bd2ddea60c122e171
cicerotcv <elm.tiago@gmail.com>
2021-10-14 03:26:20 -0300

    refactor table usage

--- frontend/frontend-dev/src/components/Table/index.tsx
+++ frontend/frontend-dev/src/components/Table/index.tsx
@@ -1,16 +1,19 @@
 import { memo, ReactNode } from "react";
 
 interface ITableProps {
+  header?: Record<string, string>;
   data: Record<string, ReactNode>[];
 }
 
-function UnmemoizedTable({ data }: ITableProps) {
+function UnmemoizedTable({ data, header }: ITableProps) {
   if (!data.length) {
     return null;
   }
 
-  const header = Object.keys(data[0]);
-  const rows = data.map((item) => Object.values(item));
+  const headerLabels = header ? Object.values(header) : Object.keys(data[0]);
+  const rows = header
+    ? data.map((row) => Object.keys(header).map((key) => row[key]))
+    : data.map((item) => Object.values(item));
 
   return (
     <div className="flex m-2">
@@ -20,7 +23,7 @@ function UnmemoizedTable({ data }: ITableProps) {
             <table className="divide-y divide-gray-200">
               <thead className="bg-gray-50">
                 <tr>
-                  {header.map((cell, cellIndex) => (
+                  {headerLabels.map((cell, cellIndex) => (
                     <th key={"header__" + cellIndex} className="p-3">
                       {cell}
                     </th>
@@ -73,5 +76,38 @@ function UnmemoizedTable({ data }: ITableProps) {
     | cell 00 | cell 10 | cell 20  |
     | cell 01 | cell 11 | cell 21  |
     | cell 02 | cell 12 | cell 22  |
+  *
+  * 
+  * Or
+  * 
+  * ```typescript
+  * const columns = {
+  *   h1: "Header 1",
+  *   h2: "Header 2",
+  *   hx: "Header x"
+  * }
+  * 
+  * const data = [
+  *   { h1: "cell 00", h2: "cell 10", h3: "cell 20", hx: "cell x0" },
+  *   { h1: "cell 01", h2: "cell 11", h3: "cell 21", hx: "cell x1" },
+  *   { h1: "cell 02", h2: "cell 12", h3: "cell 22", hx: "cell x2" },
+  * ]
+  * ```
+  * 
+  * ```typescriptreact
+  * <Table header={header} data={data} />
+  * ```
+  * 
+  * To render:
+  *
+      | Header 1 | Header 2 | Header x |
+      |----------|----------|----------|
+      | cell 00  | cell 10  | cell x0  |
+      | cell 01  | cell 11  | cell x1  |
+      | cell 02  | cell 12  | cell x2  |
+  *
+  * Note that using `header` property you're able to select which
+  * columns will be shown and even rename them.
+  * 
  */
 export const Table = memo(UnmemoizedTable);
