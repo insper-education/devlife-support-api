b1296bed7528efd2562e9c9446794c0e231207c6
Igor Montagner <igordsm@gmail.com>
2021-10-20 21:10:20 -0300

    Add docker compose for production builds. (#33)

--- Dockerfile
+++ Dockerfile
@@ -2,5 +2,7 @@ FROM python:3
 RUN mkdir -p /app
 COPY . /app
 WORKDIR /app
+RUN pip install --upgrade pip
 RUN python -m pip install -r requirements.txt
+
 CMD python manage.py migrate && python manage.py runserver 0.0.0.0:8000
--- devlife_support/settings.py
+++ devlife_support/settings.py
@@ -86,12 +86,25 @@ WSGI_APPLICATION = "devlife_support.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+development = bool(int(os.getenv("DEV", "1")))
+if development:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
     }
-}
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.postgresql",
+            "NAME": os.getenv("SQL_DATABASE"),
+            "USER": os.getenv("SQL_USER"),
+            "PASSWORD": os.getenv("SQL_PASSWORD"),
+            "HOST": "db",
+            "PORT": 5432
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
