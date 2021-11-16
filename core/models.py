eb36347c74b9f21f07d2c9106b15f724d33d5221
Igor Montagner <igordsm@gmail.com>
2021-11-16 10:48:56 -0300

    Fix HTTP 500 when slug > 50. Now slug size is 255

--- core/models.py
+++ core/models.py
@@ -67,7 +67,7 @@ class Teaches(models.Model):
 
 class Exercise(models.Model):
     offering = models.ForeignKey(Offering, on_delete=models.CASCADE)
-    slug = models.SlugField()
+    slug = models.SlugField(max_length=255)
     url = models.URLField()
     topic = models.CharField(max_length=1024)
     group = models.CharField(max_length=1024)
